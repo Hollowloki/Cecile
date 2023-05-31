0073a9a444490d33f8bb84ef29fbe53b111acaee
luki <letitbegos@gmail.com>
2023-05-31 22:48:45 +0800

    ast impl

--- LLVM/chunk.h
+++ LLVM/chunk.h
@@ -3,20 +3,24 @@
 
 #include "common.h"
 #include "memory.h" 
-
+#include "value.h"
 typedef enum {
   OP_RETURN,
+  OP_CONSTANT,
 } Opcode;
 
 typedef struct {
   int count;
   int capacity;
   uint8_t* code;
+  int* lines;
+  ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
 
