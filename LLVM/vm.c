25993a3a9eedf20b484e11df47ea66dc63f17967
luki <letitbegos@gmail.com>
2023-06-02 13:16:20 +0800

    Execution tracking

--- LLVM/vm.c
+++ LLVM/vm.c
@@ -1,8 +1,10 @@
 #include "chunk.h"
 #include "common.h"
+#include "value.h"
 #include "vm.h"
 #include <stdint.h>
-
+#include <stdio.h>
+#include "debug.h"
 VM vm;
 
 void initVM() {
@@ -15,10 +17,21 @@ void freeVM() {
 
 static InterpretResult run() {
   #define READ_BYTE() (*vm.ip++)
+  #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
   for (;;) {
+    #ifdef DEBUG_TRACE_EXECUTION
+      disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+    #endif
+
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
+      case OP_CONSTANT: {
+        Value constant = READ_CONSTANT();
+        printValue(constant);
+        printf("\n");
+        break;
+      }
       case OP_RETURN: {
          return INTERPRET_OK;
       }
@@ -26,6 +39,7 @@ static InterpretResult run() {
   }
 
   #undef READ_BYTE
+  #undef READ_CONSTANT
 }
 
 InterpretResult interpret(Chunk *chunk) {
