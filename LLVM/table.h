94b995e92c1fa63a8e5b7be36876184ff10a6475
luki <letitbegos@gmail.com>
2023-06-15 17:32:51 +0800

    hashtable impl

---
+++ LLVM/table.h
@@ -0,0 +1,24 @@
+#ifndef clox_table_h
+#define clox_table_h
+
+#include "object.h"
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+  ObjString* key;
+  Value value;
+} Entry;
+
+typedef struct {
+  int count;
+  int capacity;
+  Entry* entries;
+} Table;
+
+
+void initTable(Table* table);
+void freeTable(Table* table);
+void tableSet(Table* table, ObjString* key, Value value);
+
+#endif
